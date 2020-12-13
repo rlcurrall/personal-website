f3824d4f6ca519a62e1901c9483ea954c8e56c79
Robb Currall <rlcurra@castlebranch.com>
2020-12-13 11:15:51 -0500

    Add css to purgecss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ['./src/**/*.{vue,js,ts,jsx,tsx}'],
+  purge: ['./src/**/*.{vue,js,ts,jsx,tsx,css}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
