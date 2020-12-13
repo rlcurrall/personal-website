ec6c43b24c501e93454c92990870a38c694ff1d5
Robb Currall <rlcurra@castlebranch.com>
2020-12-13 11:12:36 -0500

    Update intro and add resume download

--- src/views/CV.vue
+++ src/views/CV.vue
@@ -4,7 +4,18 @@
     <p class="text-red-500 py-4 text-xs italic">
       <i class="fas fa-tools" /> Under Construction
     </p>
+    <a :href="`https://drive.google.com/uc?export=download&id=${resumeId}`">
+      PDF Download
+    </a>
   </main>
 </template>
 
-<style></style>
+<script>
+export default {
+  setup() {
+    const resumeId = '1hUInP7VyxqocZNsxG7lPbzoL9wgbK1ru'
+
+    return { resumeId }
+  },
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,36 +3,39 @@
     class="text-white flex flex-col items-start p-8 container mx-auto
     md:flex-row md:px-0"
   >
-    <div class="mr-8 self-center md:self-start">
+    <div class="mx-0 mb-10 self-center md:mr-8 md:my-0 md:self-start">
       <img
         alt="Robb Currall"
         src="@/assets/profile.jpg"
         class="border border-green-500 rounded-full w-32"
       />
     </div>
-    <article>
+    <article class="space-y-8 max-w-3xl text-justify">
       <h1>
         Welcome!
       </h1>
-      <p class="my-8">
-        I'm <b>Robb Currall</b>, a Christian, husband, and a dungeon master. I
-        make a living developing software.
-      </p>
-      <p class="my-8">
-        I graduated in December 2018 with a degree in computational science from
-        the University of South Carolina Beaufort.
+      <p>
+        I am <b>Robb Currall</b>, a Christian, husband, and a dungeon master. I
+        make a living developing software. I'm currently a Lead Software
+        Engineer at CastleBrach, a company offering background screening, drug
+        testing, and compliance tracking for employers.
       </p>
       <p>
-        I am currently working as a full-stack developer, developing web
-        applications using Vue and Laravel; though I have started to find a
-        passion for systems programming with Rust.
+        My expertise spans enterprise and solution architecture, API design,
+        general web application development, data management, and developing
+        authentication solutions.
+      </p>
+      <p>
+        In my current position, I'm working on a product set that aims to help
+        organizations mitigate the risk of infectious diseases through
+        monitoring employee temperature and symptom data, providing alerts and
+        automated isolation/quarantine procedures.
+      </p>
+      <p>
+        I am a dungeon master and player in a weekly game of Dungeons and
+        Dragons (5e), where cleverness, creativity, and a bit of luck are always
+        present.
       </p>
     </article>
   </main>
 </template>
-
-<style scoped>
-article {
-  max-width: 42rem;
-}
-</style>
