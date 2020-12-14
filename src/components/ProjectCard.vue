9e64498cc1b166b0cc9ffa35d470060df688fb61
Robb Currall <rlcurra@castlebranch.com>
2020-12-14 13:17:19 -0500

    Darken a bit

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,14 +1,14 @@
 <template>
     <a
         class="
-            flex flex-col items-center w-64 h-64 m-4 bg-gray-200 overflow-hidden
+            flex flex-col items-center w-64 h-64 m-4 bg-gray-300 overflow-hidden
             rounded border-0 text-black shadow-xl hover:no-underline
             focus:outline-none focus:ring focus:border-green-300 scale-up-center
           "
         :href="project.url"
         target="_blank"
     >
-        <div class="py-1 bg-gray-300 w-full text-center text-yellow-600">
+        <div class="py-1 bg-gray-500 w-full text-center text-yellow-500">
             {{ project.title }}
         </div>
         <div class="p-2 flex-grow text-sm">
