452788dd62008f851e76566e7411ed17eb9feef5
Robb Currall <rlcurra@castlebranch.com>
2020-12-14 22:50:14 -0500

    convert to module based

--- server.js
+++ server.js
@@ -1,19 +1,15 @@
-/* NODE MODULES */
-const path = require('path');
-const http = require('http');
-const helmet = require('helmet');
-const express = require('express');
+import path from 'path';
+import http from 'http';
+import helmet from 'helmet';
+import express from 'express';
 
-/* GLOBAL VARIABLES */
-const publicPath = path.join(__dirname, '/dist');
+const publicPath = path.join(process.cwd(), '/dist');
 const port = process.env.PORT || 3000;
 
-/* SERVER CONFIG */
-let app = express()
+const app = express()
     .disable('x-powered-by')
     .use(helmet())
     .use(express.static(publicPath))
     .get('*', (req, res) => res.sendFile(`${publicPath}/index.html`));
 
-/* SET PORT LISTENER */
 http.createServer(app).listen(port, () => console.log(`Server is up on port ${port}`));
